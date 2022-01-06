4cb197d40b8cac3d55c6d7333a109a697e293d1a
sandraskolarczyk <93612381+sandiskolarczyk@users.noreply.github.com>
2022-01-06 16:52:43 +0000

    do objects and attempt combo task

--- index.js
+++ index.js
@@ -41,7 +41,9 @@ export function removeItem(array, index) {
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+  return { ...object, name: newName };
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -49,7 +51,9 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+  return { ...object, needsACupOfTea: !object.needsACupOfTea };
+}
 
 // Combo Time!!
 
@@ -59,4 +63,21 @@ export function toggleTeaStatus(object) {}
 //    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+
+// PLAN
+// output: an array which will have the updated value of the item.completed at a specified index
+// spreading the array up to the index/element we want to change
+// then we are at the specified index
+// access this element
+// change the property
+
+// change the value of one of the objects of the property to be the opposite of what it was
+export function toggleListItemCompleted(array, index) {
+  return [...array.slice(0, index), ...array[index], completed];
+  const newArray = [...array];
+  let item = newArray[index];
+  console.log(item);
+  item.completed = !item.completed;
+  console.log(item);
+  return newArray;
+}
